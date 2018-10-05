402c973f85589c35cc50556c1c079383782b7c8c
jakobvaa <jakob.vaardal@gmail.com>
2018-10-05 14:08:12 +0200

    Used 3rd party library to create swipe to delete functionality

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import Task from '../components/Task.js'
+import Swipeout from 'react-native-swipeout';
 
+let swipeoutBtns = [
+  {
+    text:'Done',
+    backgroundColor: "#00ff00"
+    //onPress = function to change state, task has been completed
+  }
+]
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Todude',
   };
+
 
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <ScrollView style={styles.container} backgroundColor = "transparent">
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <Swipeout right = {swipeoutBtns}>
+          <Task text = "Buggydi Woop"/>
+        </Swipeout>
       </ScrollView>
     );
   }
