a7eee6d8ce0cdb7d8cc9fddcbcd019c9fb7d69e6
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-06 14:04:04 +0200

    Cleaned App.js

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
-        <AppLoading
+        <AppLoading  // (Very) Useful for caching assets thats required for the app to render
           startAsync={this._loadResourcesAsync}
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
@@ -27,29 +27,23 @@ export default class App extends React.Component {
     }
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
+  _loadResourcesAsync = async () => {  // Burde vi implementere Async-storage her?
+    /*const asset = [
+      require('./assets/asset1.type'),
+      require('./assets/asset2.type'),
+    ];
+
+    const cacheAssets = Assets.map(asset => asset.fromModule(asset).downloadAsync());
+    return Promise.all(cacheAssets) */
+    console.log(Asset.fromModule(require('./assets/images/icon.png')).downloadAsync().uri);
   };
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
+  _handleLoadingError = error => {  // Called if the startAsync-prop returns an error
     console.warn(error);
+    
   };
 
-  _handleFinishLoading = () => {
+  _handleFinishLoading = () => {  // Called after onError and startAsync
     this.setState({ isLoadingComplete: true });
   };
 }
