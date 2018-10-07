2ea994ec08d98b37795708da8cc679b2aa8e6bdd
jakobvaa <jakob.vaardal@gmail.com>
2018-10-07 17:49:10 +0200

    Finished the task component

--- components/Task.js
+++ components/Task.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet} from 'react-native';
+import Swipeout from 'react-native-swipeout';
+
 
 export default class Task extends Component {
+
+  constructor(props){
+    super(props);
+    this.swipeoutBtns = [
+      {
+        text:'Done',
+        backgroundColor: "#2ecc71",
+        //onPress = function to change state, task has been completed
+        onPress: () => this.props.click(props.id)
+
+      }
+    ]
+  }
+
   render() {
     return (
-      <View style = {styles.container}>
-        <Text style = {[styles.fineText, styles.font]}>{this.props.text}</Text>
-      </View>
+      <Swipeout autoClose = {true} style = {styles.margin} right = {this.swipeoutBtns}>
+        <View style = {styles.container}>
+          <Text style = {[styles.fineText, styles.font]}>{this.props.text}</Text>
+        </View>
+      </Swipeout>
     );
   }
 }
@@ -19,5 +37,8 @@ const styles = StyleSheet.create({
   },
   font:{
     fontSize: 30
+  },
+  margin:{
+    marginBottom:5
   }
 })
