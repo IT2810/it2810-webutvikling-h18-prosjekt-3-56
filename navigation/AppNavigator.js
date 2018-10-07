f3a256ae4a740da3653a8ae133a59aafe6bda06a
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-07 15:34:41 +0200

    #11 - Deleted unnesecary files

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,60 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 
-export default createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+// ES6 syntax: { HomeScreen } is the same as { HomeScreen: HomeScreen}.
+const HomeStack = createStackNavigator({ HomeScreen });
+
+/*Recall: HomeStack becomes the screen that Tabnavigator renders.
+ Tab options must therefore be visible for TabNavigator*/
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({ LinksScreen });
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Links',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({ SettingsScreen });
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+/* Acts the same way as stackNavigator, but with the inclusion of a Tabbar 
+that automticly implements navigation between routes (screens)*/
+export default createBottomTabNavigator({
+  HomeStack,  // The different routes
+  LinksStack,
+  SettingsStack,
 });
+// Does it have to be a Stack-component? Or could it be an ordinary component, like Homescreen?
