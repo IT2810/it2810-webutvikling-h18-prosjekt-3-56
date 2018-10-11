07e6ccef7368c6b7ab7a06227ff717e3667b0c58
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-11 16:47:09 +0200

    #5-Shortened code

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -4,8 +4,6 @@ import Colors from '../constants/Colors';
 
 export default class TabBarIcon extends React.Component {
 
-  e = React.createElement;
-
   createProps = () => ({
     name: this.props.name,
     size: 26,
@@ -14,15 +12,16 @@ export default class TabBarIcon extends React.Component {
     })
 
   icon = () => {
+    const e = React.createElement;
     const {type} = this.props
     if (type === 'Ionicons'){
-      return this.e(Ionicons, this.createProps());
+      return e(Ionicons, this.createProps());
     }
     else if (type === 'FontAwesome'){
-      return this.e(FontAwesome, this.createProps());
+      return e(FontAwesome, this.createProps());
     }
     else if (type === 'Octicons'){
-      return this.e(Octicons, this.createProps());
+      return e(Octicons, this.createProps());
     }
   }
 
