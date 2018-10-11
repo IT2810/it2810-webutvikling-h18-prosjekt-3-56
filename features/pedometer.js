3e0706cd55d3c7fed1f4e735a5e3d3b21f80e78a
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-11 17:37:09 +0200

    #7-Changed to appropriate name

---
+++ features/pedometer.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {} from 'expo';
+
+class pedometer extends Component{
+	/* Denne komponenten setter sammen statusbar, foot-icon, 
+	implementer aksess til accelerometer og styler det*/
+}
