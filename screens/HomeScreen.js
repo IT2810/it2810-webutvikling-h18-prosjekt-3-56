deaafc8b76f4bbb969e2e878e1347b5a2d59f752
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-17 11:59:00 +0200

    Fixed Component name

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import Pedometer from '../features/motivatingPedometer.js';
+import Pedometer from '../features/MotivatingPedometer.js';
 
 export default class HomeScreen extends React.Component {
 
