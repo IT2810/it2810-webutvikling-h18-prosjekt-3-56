1a8d20583e8bdd861dfefe6a00edc846cd42e9c0
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-18 18:15:54 +0200

    #26-Did some snapshot test

--- __tests__/MotivatingPedometer-test.js
+++ __tests__/MotivatingPedometer-test.js
@@ -2,3 +2,22 @@ import React from 'react';
 import Pedometer from '../features/MotivatingPedometer.js';
 import renderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import {ProgressCircle }  from 'react-native-svg-charts';
+
+it("renders correctly", () => {
+	const tree = renderer.create(<Pedometer />).toJSON();
+	expect(tree).toMatchSnapshot();
+})
+
+it("renders progressCircle correct", () => {
+	const tree = renderer
+	.create(
+		<ProgressCircle 
+			style = {{height: 350}}
+			progress = {0.7 / 1}
+			progressColor = {'black'}
+		/>
+	)
+	.toJSON();
+	expect(tree).toMatchSnapshot();
+})
