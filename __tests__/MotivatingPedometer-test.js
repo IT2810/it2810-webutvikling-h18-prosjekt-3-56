cc2969060660de638361c93228838f8dc643abc3
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-18 15:36:40 +0200

    #26-Added testing file

---
+++ __tests__/MotivatingPedometer-test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Pedometer from '../features/MotivatingPedometer.js';
+import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
