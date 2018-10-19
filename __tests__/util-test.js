a79ce77fb57930853631c7b2671143f1553d6ef1
Scott Gullaksen <scotty_boy_96@hotmail.com>
2018-10-19 15:47:59 +0200

    Added test for validation method

--- __tests__/util-test.js
+++ __tests__/util-test.js
@@ -1,4 +1,4 @@
-import {removeItem} from '../util/util.js';
+import {removeItem, isPostitveInteger} from '../util/util.js';
 list = [{"id":0}, {"id":1}, {"id":2}, {"id":3}];
 
 
@@ -7,3 +7,16 @@ test("removeItem function works", () => {
   expect(removeItem(0,list)).toEqual([{"id":1}, {"id":2}, {"id":3}]);
   expect(removeItem(2.5,list)).toEqual([{"id":1}, {"id":2}, {"id":3}]);
 });
+
+test('validation function works', () => {
+  expect(isPostitveInteger(0)).toBe(true);
+  expect(isPostitveInteger(-1)).toBe(false);
+  expect(isPostitveInteger(1.7)).toBe(false);
+  expect(isPostitveInteger(1000)).toBe(true);
+  expect(isPostitveInteger('100')).toBe(true);
+  expect(isPostitveInteger('-145')).toBe(false);
+  expect(isPostitveInteger('0')).toBe(true);
+  expect(isPostitveInteger('kjfdsdf')).toBe(true);
+  expect(isPostitveInteger('1.89')).toBe(true);
+  expect(isPostitveInteger('18*46 - 12')).toBe(false);
+})
